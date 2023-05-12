f53f15e39c810b628ce2cfc8c5ea4694ace6a35b
jeffreytong123 <38090716+jeffreytong123@users.noreply.github.com>
2023-05-12 21:35:10 +1200

    Update Queries.sql
    added queries to find complete statistics for each individual NBA player

--- Queries.sql
+++ Queries.sql
@@ -1,7 +1,16 @@
 -- 1.1 Select the names of all the products in the store.
 
---All of these queries will be written for both the Regular Season and Postseason
+--Some of these queries may look similar as they will be written for both the Regular Season and Postseason
 
+
+--This is used to find a specific players statistics for the entire season (When writing player name you must use a capital letter on their first and last name and first three letters of their team name etc. 'Luka DoncicDAL')
+SELECT "NAME", "POS", "GP", "MIN", "PTS", "FGM", "FGA", "FG%", "3PM", "3PA", "3P%", "FTM", "FTA", "FT%" "REB", "AST", "STL", "BLK", "TO"
+FROM nba_regular_season_2020_21
+WHERE "NAME" = 'ANY PLAYER NAME HERE'
+
+SELECT "NAME", "POS", "GP", "MIN", "PTS", "FGM", "FGA", "FG%", "3PM", "3PA", "3P%", "FTM", "FTA", "FT%" "REB", "AST", "STL", "BLK", "TO"
+FROM nba_postseason_2020_21
+WHERE "NAME" = 'ANY PLAYER NAME HERE'
 
 --Find the average amount of games an NBA player plays
 SELECT 'NAME', AVG("GP") AS games_played
